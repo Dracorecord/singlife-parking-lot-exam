ab9ea2ed24568b3a135d2b3a3f080865c97e767b
kayle_kinley_staana <kayle.sta.ana@xurpas.com>
2022-07-07 04:40:56 +0800

    new change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
-import ParkingLot from "./pages/ParkingLot";
-
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="max-w-screen-lg m-auto">
-        <ParkingLot />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
